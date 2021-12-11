9fe0fa0e01d848a7d87e74919f194079d68c69ff
abe-mw.github.io <01885033@student.commnet.edu>
2021-12-11 00:41:44 -0500

    Javascript

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+//log to console
+console.log('Hi Im Abe');
+console.log("4321")
+//Console error
+console.error ("Dang dude!")
+//var
+var name =("Abe")
+
+var fname = prompt ("What is your FIRST name?");
+var lname = prompt ("What is your LAST name?");
+console.log("Welcome" + " " + fname + " " + lname)
